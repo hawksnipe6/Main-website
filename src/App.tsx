0d57c854b93ec97b19c26362445e6b6d502a0526
hawksnipe6 <designwithabeer@gmail.com>
2026-04-27 09:13:43 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import { Nav } from './components/Nav'
+import { Hero } from './components/Hero'
+import { Marquee } from './components/Marquee'
+import { About } from './components/About'
+import { Services } from './components/Services'
+import { Disciplines } from './components/Disciplines'
+import { Process } from './components/Process'
+import { Clients } from './components/Clients'
+import { Pricing } from './components/Pricing'
+import { Faq } from './components/Faq'
+import { Cta } from './components/Cta'
+import { Footer } from './components/Footer'
+import { CustomCursor } from './components/CustomCursor'
+import { ScrollProgress } from './components/ScrollProgress'
+
+export default function App() {
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible')
+          }
+        })
+      },
+      { threshold: 0.1, rootMargin: '0px 0px -40px 0px' }
+    )
+
+    document.querySelectorAll('.reveal').forEach((el) => observer.observe(el))
+
+    return () => observer.disconnect()
+  }, [])
+
+  return (
+    <>
+      <CustomCursor />
+      <ScrollProgress />
+      <Nav />
+      <main>
+        <Hero />
+        <Marquee />
+        <About />
+        <Services />
+        <Disciplines />
+        <Process />
+        <Clients />
+        <Pricing />
+        <Faq />
+        <Cta />
+      </main>
+      <Footer />
+    </>
+  )
+}
